a251aa251114e1738766f55667f937779c5f464e
kortirso <kortirso@gmail.com>
2018-10-29 10:13:35 +0200

    add reverse function, modify functions

--- lib/open_street_map.ex
+++ lib/open_street_map.ex
@@ -12,7 +12,58 @@ defmodule OpenStreetMap do
       :world
 
   """
-  def fetch(url) do
+  def search(args) do
+    call("search", args)
+  end
+
+  def reverse(args) do
+    call("reverse", args)
+  end
+
+  defp call(type, args) do
+    generate_url(type, args)
+    |> prepare_url(type)
+    |> fetch
+  end
+
+  defp generate_url(type, args) do
+    Enum.filter(args, fn({key, _value}) -> Enum.member?(valid_args(type), key) end)
+    |> List.foldl("", fn({key, value}, acc) -> acc <> add_to_options(key, to_string(value), acc) end)
+  end
+
+  defp valid_args(type) do
+    case type do
+      "search" -> [:q, :format, :addressdetails, :extratags, :namedetails, :viewbox, :bounded, :exclude_place_ids, :limit, :accept_language, :email]
+      "reverse" -> [:format, :lat, :lon, :zoom, :addressdetails, :extratags, :namedetails, :accept_language, :email]
+      _ -> []
+    end
+  end
+
+  defp add_to_options(key, value, "") do
+    "?" <> key_value_param(key, modify_search(value))
+  end
+
+  defp add_to_options(key, value, _acc) do
+    "&" <> key_value_param(key, modify_search(value))
+  end
+
+  defp modify_search(value) do
+    String.replace(value, ~r/\s+/, "+")
+  end
+
+  defp key_value_param(:accept_language, value) do
+    "accept-language=" <> value
+  end
+
+  defp key_value_param(key, value) do
+    "#{key}=#{value}"
+  end
+
+  defp prepare_url(url, type) do
+    type <> url
+  end
+
+  defp fetch(url) do
     base_url = "https://nominatim.openstreetmap.org/"
     headers = ["Accept": "Application/json; Charset=utf-8"]
     options = [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
@@ -24,44 +75,5 @@ defmodule OpenStreetMap do
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
     end
-  end
-
-  def search(args) do
-    generate_url(args)
-    |> prepare_url("search")
-    |> fetch
-  end
-
-  def generate_url(args) do
-    Enum.filter(args, fn({key, _value}) -> Enum.member?(valid_search_args(), key) end)
-    |> List.foldl("", fn({key, value}, acc) -> acc <> add_to_options(key, to_string(value), acc) end)
-  end
-
-  def valid_search_args do
-    [:q, :format, :addressdetails, :extratags, :namedetails, :viewbox, :bounded, :exclude_place_ids, :limit, :accept_language, :email]
-  end
-
-  def add_to_options(key, value, "") do
-    "?" <> key_value_param(key, modify_search(value))
-  end
-
-  def add_to_options(key, value, _acc) do
-    "&" <> key_value_param(key, modify_search(value))
-  end
-
-  def key_value_param(:accept_language, value) do
-    "accept-language=" <> value
-  end
-
-  def key_value_param(key, value) do
-    "#{key}=#{value}"
-  end
-
-  def modify_search(value) do
-    String.replace(value, ~r/\s+/, "+")
-  end
-
-  def prepare_url(type, url) do
-    type <> url
   end
 end
