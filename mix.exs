bcd0520f4c4c10d8de19d21cf7af085a0787fa1d
kortirso <kortirso@gmail.com>
2018-10-29 10:24:03 +0200

    modify project information

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule OpenStreetMap.MixProject do
   use Mix.Project
 
+  @description """
+    Integration with OpenStreetMap API
+  """
+
   def project do
     [
       app: :open_street_map,
       version: "0.9.0",
       elixir: "~> 1.7",
+      name: "OpenStreetMap",
+      description: @description,
+      source_url: "https://github.com/kortirso/open_street_map",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -22,6 +30,15 @@ defmodule OpenStreetMap.MixProject do
   defp deps do
     [
       {:httpoison, "1.4.0"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README"],
+      maintainers: ["Anton Bogdanov"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kortirso/open_street_map"}
     ]
   end
 end
