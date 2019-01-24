1fa66c7054772b80fa426a1f7eea6560f5715bef
kortirso <kortirso@gmail.com>
2019-01-24 21:20:20 +0300

    release 0.9.8

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule OpenStreetMap.MixProject do
   def project do
     [
       app: :open_street_map,
-      version: "0.9.7",
+      version: "0.9.8",
       elixir: "~> 1.7",
       name: "OpenStreetMap",
       description: @description,
